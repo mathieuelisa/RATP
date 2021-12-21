7901e9a1f1715bb4f6b3b722f68d9faee61166ef
mathieuelisa <mathieu.elisa@gmail.com>
2021-12-21 22:24:03 +0100

    Adding form and configuration

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,62 @@
+import { useState } from "react";
 import "./Styles.scss";
 
 function App() {
+  const [state, setState] = useState({
+    numberOfParticipants: "",
+    quittanceOf35: "",
+    quittanceOf50: "",
+    quittanceOf60: "",
+  });
+
+  const handleChange = (evt) => {
+    setState({
+      ...state,
+      [evt.target.name]: evt.target.value,
+    });
+  };
+
+  console.log(state);
+
   return (
     <div className="App">
       <h1>RATP APP</h1>
+      <div>
+        <input
+          type="number"
+          value={state.numberOfParticipants}
+          onChange={handleChange}
+          name="numberOfParticipants"
+        />
+        Nombre de participants
+      </div>
+      <div>
+        <input
+          type="number"
+          value={state.quittanceOf35}
+          onChange={handleChange}
+          name="quittanceOf35"
+        />
+        Quittance de 35 euros
+      </div>
+      <div>
+        <input
+          type="number"
+          value={state.quittanceOf50}
+          onChange={handleChange}
+          name="quittanceOf50"
+        />
+        Quittance de 50 euros
+      </div>
+      <div>
+        <input
+          type="number"
+          value={state.quittanceOf60}
+          onChange={handleChange}
+          name="quittanceOf60"
+        />
+        Quittance de 60 euros
+      </div>
     </div>
   );
 }
