159d5e03503a160b3622d979bd516b75ccd9a535
mathieuelisa <mathieu.elisa@gmail.com>
2021-12-24 22:32:00 +0100

    Adding react reveal and styling the app

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import "./Styles.scss";
+import logo from "../Assets/ratp.png";
+import { Zoom, Fade } from "react-reveal";
 
 function App() {
   const [state, setState] = useState({
@@ -24,13 +26,13 @@ function App() {
   const integerOf60 = Number(state.quittanceOf60);
 
   // Function get an average
-  function getAverage(oneLevel, secondLevel, thirdLevel) {
+  const getAverage = (oneLevel, secondLevel, thirdLevel) => {
     let resultOfFirstLevel = oneLevel * 35;
     let resultOfSecondLevel = secondLevel * 50;
     let resultOfThirdLevel = thirdLevel * 60;
 
     return resultOfFirstLevel + resultOfSecondLevel + resultOfThirdLevel;
-  }
+  };
 
   let myResult =
     getAverage(integerOf35, integerOf50, integerOf60) / integerParticipants;
@@ -39,48 +41,99 @@ function App() {
     setStateResult(myResult / state.numberOfParticipants);
   };
 
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  const myCurrentDate = new Date().toLocaleDateString("fr-FR", options);
+  console.log(myCurrentDate);
+
   return (
     <div className="App">
-      <h1>RATP APP</h1>
-      <div>
-        <input
-          type="number"
-          value={state.numberOfParticipants}
-          onChange={handleChange}
-          name="numberOfParticipants"
-        />
-        Nombre de participants
+      <Zoom duration={1000}>
+        <div className="logo">
+          <img src={logo} alt="logo" className="logo-image" />
+        </div>
+      </Zoom>
+      <div className="wrapper">
+        <Fade left duration={2000}>
+          <div className="inputData">
+            <label className="inputData-label">
+              <input
+                type="number"
+                value={state.numberOfParticipants}
+                onChange={handleChange}
+                name="numberOfParticipants"
+                className="inputData-each"
+              />
+              <p className="inputData-text">Nombre de participants</p>
+            </label>
+          </div>
+        </Fade>
+
+        <Fade right duration={2000}>
+          <div className="inputData">
+            <label className="inputData-label">
+              <input
+                type="number"
+                value={state.quittanceOf35}
+                onChange={handleChange}
+                name="quittanceOf35"
+                className="inputData-each"
+              />
+              <p className="inputData-text"> Quittances de 35 euros</p>
+            </label>
+          </div>
+        </Fade>
+
+        <Fade left duration={2000}>
+          <div className="inputData">
+            <label className="inputData-label">
+              <input
+                type="number"
+                value={state.quittanceOf50}
+                onChange={handleChange}
+                name="quittanceOf50"
+                className="inputData-each"
+              />
+              <p className="inputData-text"> Quittances de 50 euros</p>
+            </label>
+          </div>
+        </Fade>
+
+        <Fade right duration={2000}>
+          <div className="inputData">
+            <label className="inputData-label">
+              <input
+                type="number"
+                value={state.quittanceOf60}
+                onChange={handleChange}
+                name="quittanceOf60"
+                className="inputData-each"
+              />
+              <p className="inputData-text"> Quittances de 60 euros</p>
+            </label>
+          </div>
+        </Fade>
+        <button onClick={handleResult} className="result-button">
+          LET'S GO
+        </button>
+        {!stateResult ? (
+          ""
+        ) : (
+          <>
+            {" "}
+            <h2 className="result">
+              Prime de{" "}
+              <span className="result-number">{Math.floor(stateResult)}</span>{" "}
+              euros pour la journée du {""}
+              {myCurrentDate}.
+            </h2>
+          </>
+        )}
       </div>
-      <div>
-        <input
-          type="number"
-          value={state.quittanceOf35}
-          onChange={handleChange}
-          name="quittanceOf35"
-        />
-        Quittances de 35 euros
-      </div>
-      <div>
-        <input
-          type="number"
-          value={state.quittanceOf50}
-          onChange={handleChange}
-          name="quittanceOf50"
-        />
-        Quittances de 50 euros
-      </div>
-      <div>
-        <input
-          type="number"
-          value={state.quittanceOf60}
-          onChange={handleChange}
-          name="quittanceOf60"
-        />
-        Quittances de 60 euros
-      </div>
-      <button onClick={handleResult}>GO</button>
-      {/* <h2>Resultat: {myResult}</h2> */}
-      <h2>Le resultat est de : {Math.floor(stateResult)}</h2>
     </div>
   );
 }
